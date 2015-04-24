08c7d94bd0d97b65e16f9f2333e8626f3a2d18ec
Dave Kellam <dave@eightface.com>
2015-04-23 21:55:52 -0400

    Base theme

---
+++ assets/js/src/worldstrides.js
@@ -0,0 +1,13 @@
+/**
+ * WorldStrides
+ * http://www.worldstrides.com
+ *
+ * Copyright (c) 2015 WorldStrides
+ * Licensed under the GPLv2+ license.
+ */
+ 
+ ( function( window, undefined ) {
+	'use strict';
+
+
+ } )( this );
