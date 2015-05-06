51ea1f12bc3d94e2ab21837826789c8453d8601a
Dave Kellam <dave@eightface.com>
2015-05-06 18:58:00 -0400

    Adding support for various theme options, and formatting

--- functions.php
+++ functions.php
@@ -9,10 +9,10 @@
  * theme's file, so the child theme functions would be used.
  *
  * @package WorldStrides
- * @since 0.1.0
+ * @since   0.1.0
  */
- 
- // Useful global constants
+
+// Useful global constants
 define( 'WS_VERSION', '0.1.0' );
 define( 'WS_PATH', dirname( __FILE__ ) . '/' );
 define( 'WS_IMAGE_URL', get_template_directory_uri() . '/assets/images/' );
@@ -40,29 +40,36 @@ new WS_Custom_Post_Type( 'lead' );
 new WS_Custom_Post_Type( 'leadership' );
 
 
- /**
-  * Set up theme defaults and register supported WordPress features.
-  */
- function ws_setup() {
-	/**
-	 * Makes WorldStrides available for translation.
-	 *
-	 * Translations can be added to the /lang directory.
-	 * If you're building a theme based on WorldStrides, use a find and replace
-	 * to change 'ws' to the name of your theme in all template files.
-	 */
+/**
+ * Set up theme defaults and register supported WordPress features.
+ */
+function ws_setup() {
 	load_theme_textdomain( 'ws', get_template_directory() . '/languages' );
- }
- add_action( 'after_setup_theme', 'ws_setup' );
 
- /**
-  * Enqueue scripts and styles for front-end.
-  */
- function ws_scripts_styles() {
+	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'post-thumbnails' );
+
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+}
+
+add_action( 'after_setup_theme', 'ws_setup' );
+
+/**
+ * Enqueue scripts and styles for front-end.
+ */
+function ws_scripts_styles() {
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_enqueue_script( 'ws', get_template_directory_uri() . "/assets/js/worldstrides{$postfix}.js", array(), WS_VERSION, true );
-		
+
 	wp_enqueue_style( 'ws', get_template_directory_uri() . "/assets/css/worldstrides{$postfix}.css", array(), WS_VERSION );
- }
- add_action( 'wp_enqueue_scripts', 'ws_scripts_styles' );
+}
+
+add_action( 'wp_enqueue_scripts', 'ws_scripts_styles' );
