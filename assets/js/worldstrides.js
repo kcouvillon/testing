87b384e05c71308d219f1db8c04f6a6bacbd47dd
Dave Kellam <dave@eightface.com>
2015-05-13 16:49:12 -0400

    Grunt CSS/JS dry run

---
+++ assets/js/worldstrides.js
@@ -0,0 +1,8 @@
+/*! WorldStrides - v0.1.0 - 2015-05-13
+ * http://www.worldstrides.com
+ * Copyright (c) 2015; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+ } )( this );
