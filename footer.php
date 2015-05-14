b8d0f85a066f3fec3b3b22b62585fa490b187898
Dave Kellam <dave@eightface.com>
2015-05-13 22:09:57 -0400

    Adding shell header/footer based on _s

--- footer.php
+++ footer.php
@@ -5,3 +5,16 @@
  * @package WorldStrides
  * @since 0.1.0
  */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
