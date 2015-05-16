2d00da9d0ceff898de8c987294f6304cf18ad7b1
Dave Kellam <dave@eightface.com>
2015-05-16 01:42:07 -0400

    Basic wp index

--- index.php
+++ index.php
@@ -8,6 +8,27 @@
 
  get_header(); ?>
 
-WorldStrides
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<h2><?php the_title(); ?></h2>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<p>Nothing found</p>
+
+		<?php endif; ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
  <?php get_footer(); ?>
