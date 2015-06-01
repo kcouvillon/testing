a9c7787c35df8082f1fb446c039915f9ef25f488
Dave Kellam <dave@eightface.com>
2015-06-01 17:03:13 -0400

    Adding blog home page shell

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This is actually the main blog page. Please see front-page.php for the traditional 'home' page
+ */
+
+ get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'partials/content' ) ?>
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<p>Nothing found</p>
+
+		<?php endif; ?>
+
+	</main>
+</div>
+
+<?php get_footer(); ?>
