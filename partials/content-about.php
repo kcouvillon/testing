bfbe40f471346abc8b1fd4a975d52272841841ee
Dave Kellam <dave@eightface.com>
2015-06-01 18:30:53 -0400

    Partial for About content

---
+++ partials/content-about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Content display for about pages
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( ! is_page( 'about') ) : ?>
+	<header class="entry-header">
+		<h1 class="entry-title">
+			<a href="<?php esc_url( get_permalink() ); ?>" rel="bookmark">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+	</header>
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+
+	<footer class="entry-footer">
+
+	</footer>
+</article>
