a3112944b7e597e2ffe51f322a54d6e2236aaa06
Dave Kellam <dave@eightface.com>
2015-06-01 18:31:34 -0400

    General About template

--- templates/page-about.php
+++ templates/page-about.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * Template Name: About - General
+ *
+ * This is the general template for the about page. Make sure this page is selected from the template drop down
+ */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php get_template_part( 'partials/about', 'header' ); ?>
+
+		<?php the_post(); ?>
+		<?php get_template_part( 'partials/content', 'about' ) ?>
+
+	</main>
+</div>
+
+<?php get_footer(); ?>
