d3e991d8c4d598d6437c3ff50d0ff6b0b3afa79b
Dave Kellam <dave@eightface.com>
2015-06-03 18:26:57 -0400

    Why WorldStrides shell

---
+++ templates/page-why-worldstrides.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Why WorldStrides
+ *
+ * Template for the Why WorldStrides page
+ *
+ * @todo figure out where the various content pieces are being stored
+ */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php the_post(); ?>
+		<?php get_template_part( 'partials/content' ) ?>
+
+
+	</main>
+</div>
+
+<?php get_footer(); ?>
