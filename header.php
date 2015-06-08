a7e2c5e23300ae280c3338e3d53ed20ebf343518
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-06-08 19:52:38 +1000

    default blog layout

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site container-fluid">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
--- partials/content-blog.php
+++ partials/content-blog.php
@@ -6,18 +6,32 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( ! is_page( 'about') ) : ?>
-	<header class="entry-header">
-		<h1 class="entry-title">
+	<header class="entry-header row">
+		<h1 class="entry-title col-md-offset-1 col-md-10">
 			<?php the_title(); ?>
 		</h1>
 	</header>
 	<?php endif; ?>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
+	<div class="entry-content row">
+		<div class="col-md-6 col-md-offset-1">
+			<?php the_content(); ?>
+		</div>
 	</div>
 
 	<footer class="entry-footer">
-
+		<div class="request-form">
+			<h2>Request Information about a WorldStrides Trip</h2>
+		</div>
+		<div class="row">
+			<article class="post col-md-4 col-md-offset-1">
+				<h2><a href="#">Previous Post</a></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
+			</article>
+			<article class="post col-md-4 col-md-offset-2">
+				<h2><a href="#">Next Post</a></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
+			</article>
+		</div>
 	</footer>
 </article>
