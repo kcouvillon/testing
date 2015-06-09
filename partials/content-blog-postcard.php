c1d29e90f78b6c25880db10755a78b8db73faefe
Dave Kellam <dave@eightface.com>
2015-06-09 14:52:03 -0400

    WP code formatting

--- partials/content-blog-postcard.php
+++ partials/content-blog-postcard.php
@@ -5,40 +5,40 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! is_page( 'about') ) : ?>
-	<header class="entry-header">
-		
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="entry-image hero">
-				<?php the_post_thumbnail(); ?>
-			</div>
-		<?php endif; ?>
+	<?php if ( ! is_page( 'about' ) ) : ?>
+		<header class="entry-header">
 
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<ul class="breadcrumbs list-unstyled list-inline">
-						<li><a href="#">Stories</a></li>
-						>
-						<li><a href="#">[Blog Type]</a></li>
-						>
-						<li><a href="#">[Month 12, 2015]</a></li>
-					</ul>
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="entry-image hero">
+					<?php the_post_thumbnail(); ?>
+				</div>
+			<?php endif; ?>
+
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<ul class="breadcrumbs list-unstyled list-inline">
+							<li><a href="#">Stories</a></li>
+							>
+							<li><a href="#">[Blog Type]</a></li>
+							>
+							<li><a href="#">[Month 12, 2015]</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<h1 class="entry-title">
+							<?php the_title(); ?>
+						</h1>
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<h1 class="entry-title">
-						<?php the_title(); ?>
-					</h1>
-				</div>
-			</div>
-		</div>
-	</header>
+		</header>
 	<?php endif; ?>
 
 	<div class="entry-body container-fluid">
-	
+
 		<div class="entry-content row">
 			<div class="col-md-9 col-lg-7 col-md-offset-1">
 				<?php the_content(); ?>
@@ -49,12 +49,14 @@
 			<div class="col-md-9 col-lg-7 col-md-offset-1">
 
 				<?php $posttags = get_the_tags(); ?>
-				<?php if ($posttags) : ?>
+				<?php if ( $posttags ) : ?>
 
 					<ul class="entry-tags list-unstyled list-inline">
-				  <?php foreach($posttags as $tag) : ?>
-				  	<li><a href="<?php echo get_tag_link($tag->term_id); ?>" class="btn btn-sm btn-default "><?php echo $tag->name; ?></a></li>
-				  <?php endforeach; ?>
+						<?php foreach ( $posttags as $tag ) : ?>
+							<li>
+								<a href="<?php echo get_tag_link( $tag->term_id ); ?>" class="btn btn-sm btn-default "><?php echo $tag->name; ?></a>
+							</li>
+						<?php endforeach; ?>
 					</ul>
 
 				<?php endif; ?>
@@ -80,14 +82,16 @@
 			<div class="row">
 				<article class="post col-md-4 col-md-offset-1">
 					<h2><a href="#">Previous Post</a></h2>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
 				</article>
 				<article class="post col-md-4 col-md-offset-2">
 					<h2><a href="#">Next Post</a></h2>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
 				</article>
 			</div>
 		</div>
 	</footer>
-	
+
 </article>
--- partials/content-blog-travelogue.php
+++ partials/content-blog-travelogue.php
@@ -5,37 +5,37 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! is_page( 'about') ) : ?>
-	<header class="entry-header entry-header-bg-hero">
-		
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="entry-image hero bg-hero">
-				<?php the_post_thumbnail(); ?>
-				<div class="screen screen40"></div>
-			</div>
-		<?php endif; ?>
+	<?php if ( ! is_page( 'about' ) ) : ?>
+		<header class="entry-header entry-header-bg-hero">
 
-		<div class="overlay-text container-fluid">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<ul class="breadcrumbs list-unstyled list-inline">
-						<li><a href="#">Stories</a></li>
-						>
-						<li><a href="#">[Blog Type]</a></li>
-						>
-						<li><a href="#">[Month 12, 2015]</a></li>
-					</ul>
-					<h1 class="entry-title">
-						<?php the_title(); ?>
-					</h1>
-					<?php if ( get_the_excerpt() ) { ?>
-						<p class="entry-excerpt"><?php the_excerpt(); ?></p>
-					<?php } ?>
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="entry-image hero bg-hero">
+					<?php the_post_thumbnail(); ?>
+					<div class="screen screen40"></div>
+				</div>
+			<?php endif; ?>
+
+			<div class="overlay-text container-fluid">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<ul class="breadcrumbs list-unstyled list-inline">
+							<li><a href="#">Stories</a></li>
+							>
+							<li><a href="#">[Blog Type]</a></li>
+							>
+							<li><a href="#">[Month 12, 2015]</a></li>
+						</ul>
+						<h1 class="entry-title">
+							<?php the_title(); ?>
+						</h1>
+						<?php if ( get_the_excerpt() ) { ?>
+							<p class="entry-excerpt"><?php the_excerpt(); ?></p>
+						<?php } ?>
+					</div>
 				</div>
 			</div>
-		</div>
 
-	</header>
+		</header>
 	<?php endif; ?>
 
 	<div class="container-fluid">
@@ -44,33 +44,35 @@
 			<div class="col-md-9 col-lg-7 col-md-offset-1">
 
 				<?php $posttags = get_the_tags(); ?>
-				<?php if ($posttags) : ?>
+				<?php if ( $posttags ) : ?>
 
 					<ul class="entry-tags list-unstyled list-inline">
-				  <?php foreach($posttags as $tag) : ?>
-				  	<li><a href="<?php echo get_tag_link($tag->term_id); ?>" class="btn btn-sm btn-default "><?php echo $tag->name; ?></a></li>
-				  <?php endforeach; ?>
+						<?php foreach ( $posttags as $tag ) : ?>
+							<li>
+								<a href="<?php echo get_tag_link( $tag->term_id ); ?>" class="btn btn-sm btn-default "><?php echo $tag->name; ?></a>
+							</li>
+						<?php endforeach; ?>
 					</ul>
 
 				<?php endif; ?>
 
 			</div>
 		</div>
-	
+
 		<div class="entry-body row">
-			
+
 			<div class="entry-content col-md-10 col-md-offset-1 col-lg-8 col-lg-push-2">
 				<?php the_content(); ?>
 			</div>
 
 			<div class="entry-author col-md-10 col-md-offset-1 col-lg-2 col-lg-pull-9">
 				<p><img src="/" width="80" height="80" class="circular bg-gray-base" /></p>
+
 				<p class="small"><?php echo get_the_author_meta( 'description' ); ?> </p>
 			</div>
 
 		</div>
 
-		
 
 	</div>
 
@@ -90,18 +92,24 @@
 			<div class="row">
 				<article class="post col-md-4 col-md-offset-1">
 					<p><?php the_post_thumbnail(); ?></p>
+
 					<p class="small"><strong>Previous Travelogue</strong> July 2015</p>
+
 					<h2><a href="#">Travelogue Title</a></h2>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
 				</article>
 				<article class="post col-md-4 col-md-offset-2">
 					<p><?php the_post_thumbnail(); ?></p>
+
 					<p class="small"><strong>Next Travelogue</strong> September 2015</p>
+
 					<h2><a href="#">Travelogue Title</a></h2>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna.</p>
 				</article>
 			</div>
 		</div>
 	</footer>
-	
+
 </article>
