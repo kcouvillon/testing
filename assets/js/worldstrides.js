0b5c2b5aaa76396e44fbc64696ba24353f078be5
AndrewMowe <andrew@team-eight.com>
2015-06-15 13:31:37 -0500

    grunt

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-06-01
+/*! WorldStrides - v0.1.0 - 2015-06-15
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( window, undefined ) {
