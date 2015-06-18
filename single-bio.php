7c567738c6a8dd90df63ec01c1f78eb22efdfbaa
Dave Kellam <dave@eightface.com>
2015-06-18 16:59:19 -0400

    Move content-bio partial into single-bio

--- single-bio.php
+++ single-bio.php
@@ -12,10 +12,35 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'partials/content', 'bio' ); ?>
+				<article <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<div class="headshot">
+								<?php // @todo replace this with specific image size when ready (differentiate between single/page) ?>
+								<?php the_post_thumbnail( 'large' ); ?>
+							</div>
+						<?php endif; ?>
+
+						<h3 class="entry-title">
+							<?php the_title(); ?>
+						</h3>
+
+						<?php $position = get_post_meta( $post->ID, 'ws_bio_position', true ); ?>
+						<?php if ( $position ) : ?>
+							<span class="entry-position"><?php echo esc_html( $position ); ?></span>
+						<?php endif; ?>
+					</header>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+
+					<footer class="entry-footer">
+						<?php // @todo should 'back to leadership/category' link go here? ?>
+					</footer>
+				</article>
 
 			<?php endwhile; ?>
 
