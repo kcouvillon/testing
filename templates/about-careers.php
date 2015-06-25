2b6c4b8ea32779760cc1c9d6de90d3aa5c7366df
Dave Kellam <dave@eightface.com>
2015-06-25 16:46:11 -0400

    Apply code standards to career templates

--- templates/about-careers.php
+++ templates/about-careers.php
@@ -21,59 +21,65 @@ get_header(); ?>
 				<button class="btn btn-primary">View our Current Openings</button>
 
 				<?php
-				$benefits_title = get_post_meta( $post->ID, 'about_careers_benefits_title', true );
+				$benefits_title       = get_post_meta( $post->ID, 'about_careers_benefits_title', true );
 				$benefits_description = get_post_meta( $post->ID, 'about_careers_benefits_description', true );
-				$benefits = get_post_meta( $post->ID, 'about_careers_benefits_list', true );
+				$benefits             = get_post_meta( $post->ID, 'about_careers_benefits_list', true );
 				?>
 				<section class="benefits">
 					<h3><?php echo apply_filters( 'the_title', $benefits_title ); ?></h3>
+
 					<p><?php echo apply_filters( 'the_content', $benefits_description ); ?></p>
 
 					<div class="benefits-wrap">
 						<?php if ( is_array( $benefits ) ) : ?>
-						<?php foreach ( $benefits as $benefit ) : ?>
-						<div class="benefit">
-							<?php
-							$image_url = $benefit['image'];
-							if ( ! $image_url ) { $image_url = 'http://placehold.it/80x80'; }
-							?>
-							<img class="benefit-img" src="<?php echo esc_url( $image_url ); ?>" alt="">
-							<span class="benefit-desc"><?php echo $benefit['description']; ?></span>
-						</div>
-						<?php endforeach; ?>
+							<?php foreach ( $benefits as $benefit ) : ?>
+								<div class="benefit">
+									<?php
+									$image_url = $benefit['image'];
+									if ( ! $image_url ) {
+										$image_url = 'http://placehold.it/80x80';
+									}
+									?>
+									<img class="benefit-img" src="<?php echo esc_url( $image_url ); ?>" alt="">
+									<span class="benefit-desc"><?php echo $benefit['description']; ?></span>
+								</div>
+							<?php endforeach; ?>
 						<?php endif; ?>
 					</div>
 				</section>
 
 				<section class="career-examples">
 					<?php
-					$examples_title = get_post_meta( $post->ID, 'about_careers_examples_title', true );
+					$examples_title       = get_post_meta( $post->ID, 'about_careers_examples_title', true );
 					$examples_description = get_post_meta( $post->ID, 'about_careers_examples_description', true );
-					$examples = get_post_meta( $post->ID, 'about_careers_examples_list', true );
+					$examples             = get_post_meta( $post->ID, 'about_careers_examples_list', true );
 					?>
 					<h3><?php echo apply_filters( 'the_title', $examples_title ); ?></h3>
+
 					<p><?php echo apply_filters( 'the_content', $examples_description ); ?></p>
 
 					<div class="example-wrap">
 						<?php if ( is_array( $examples ) ) : ?>
-						<?php foreach ( $examples as $example ) : ?>
-						<article class="example">
-							<div class="headshot">
-								<?php
-								$image_url = $example['image'];
-								if ( ! $image_url ) { $image_url = 'http://placehold.it/385x250'; }
-								?>
-								<img src="<?php echo esc_url( $image_url ); ?>" alt="">
-							</div>
-							<header class="entry-title">
-								<h3 class="entry-name"><?php echo apply_filters( 'the_title', $example['name'] ); ?></h3>
-								<span class="entry-desc"><?php echo apply_filters( 'the_title', $example['position'] ); ?></span>
-							</header>
-							<div class="entry-content">
-								<p><?php echo apply_filters( 'the_content', $example['description'] ); ?></p>
-							</div>
-						</article>
-						<?php endforeach; ?>
+							<?php foreach ( $examples as $example ) : ?>
+								<article class="example">
+									<div class="headshot">
+										<?php
+										$image_url = $example['image'];
+										if ( ! $image_url ) {
+											$image_url = 'http://placehold.it/385x250';
+										}
+										?>
+										<img src="<?php echo esc_url( $image_url ); ?>" alt="">
+									</div>
+									<header class="entry-title">
+										<h3 class="entry-name"><?php echo apply_filters( 'the_title', $example['name'] ); ?></h3>
+										<span class="entry-desc"><?php echo apply_filters( 'the_title', $example['position'] ); ?></span>
+									</header>
+									<div class="entry-content">
+										<p><?php echo apply_filters( 'the_content', $example['description'] ); ?></p>
+									</div>
+								</article>
+							<?php endforeach; ?>
 						<?php endif; ?>
 					</div>
 				</section>
