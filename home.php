ecc6d6548d3b81600392b622355de935324a3466
AndrewMowe <andrew@team-eight.com>
2015-07-04 11:48:12 -0500

    refs #19, adds static form to blog landing, make form styles modular

--- home.php
+++ home.php
@@ -94,6 +94,63 @@
 
 		</div>
 
+		<section class="clearfix ws-container learn-more">
+				<form action="#" class="ws-form">
+					<div class="left">
+						<h2 class="form-title">Ready to Learn More About Traveling with WorldStrides?</h2>
+						<ul class="form-fields list-unstyled">
+							<li class="field">
+								I am an
+								<select name="name">
+									<option value="">Educator</option>
+									<option value="">Parent</option>
+									<option value="">Student</option>
+								</select>
+							</li>
+							<li class="field">
+								Interested in
+								<select name="name">
+									<option value="">Middle School Travel</option>
+									<option value="">High School Travel</option>
+									<option value="">University Travel</option>
+								</select>
+							</li>
+							<li class="field">
+								Do you have a tour scheduled?
+								&nbsp;&nbsp;
+								<input type="radio" name="tour" id="tour-yes" value="yes" />
+								<label for="tour-yes">Yes</label>
+								&nbsp;
+								<input type="radio" name="tour" id="tour-no" value="no" />
+								<label for="tour-no">No</label>
+							</li>
+						</ul>
+					</div>
+					<div class="right">
+						<ul class="form-fields list-unstyled">
+							<li class="field field-complex">
+								<div class="field-left">
+									<input type="text" name="first_name" value="" placeholder="First Name" />
+								</div>
+								<div class="field-right">
+									<input type="text" name="last_name" value="" placeholder="Last Name" />
+								</div>
+							</li>
+							<li class="field">
+								<input type="email" name="email" value="" placeholder="Email Address" />
+							</li>
+							<li class="field">
+								<input type="tel" name="phone" value="" placeholder="Phone Number" />
+							</li>
+							<li class="field">
+								<input type="text" name="group_name" value="" placeholder="School or Group Name" />
+							</li>
+						</ul>
+						<input type="submit" name="" value="Get Info" class="btn btn-primary" />
+					</div>
+				</form>
+		</section>
+
 	</main>
 </div>
 
