1994ff7b451fc87bdf3cf225634a91d9bfd65047
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-07-07 12:46:48 -0400

    update 404 page layout; refs #8

--- 404.php
+++ 404.php
@@ -11,13 +11,13 @@ get_header(); ?>
 				<header class="section-header error-header pattern-6">
 					<div class="section-header-content">
 						<h1 class="page-title"><?php _e( 'Oops! We don’t know where that page is.', 'obsub' ); ?></h1>
+						<h4 class="description">But we know where the Louvre is!</h4>
 					</div>
 				</header>
 			</section>
 
-			<section class="error-map" style="background-image:url(https://api.tiles.mapbox.com/v4/worldstrides.b898407f/url-http%3A%2F%2Fwsbeta.co%2Fwp-content%2Fthemes%2Fworldstrides%2Fassets%2Fimages%2Fpin-orange.png\(2.3358607292175293,48.86101631231847\)/2.3358607292175293,48.85810631231847,15/1280x1280.png?access_token=pk.eyJ1Ijoid29ybGRzdHJpZGVzIiwiYSI6ImNjZTg3YjM3OTI3MDUzMzlmZmE4NDkxM2FjNjE4YTc1In0.dReWwNs7CEqdpK5AkHkJwg);">
+			<section class="error-map" style="background-image:url(https://api.tiles.mapbox.com/v4/worldstrides.b898407f/url-http%3A%2F%2Fwsbeta.co%2Fwp-content%2Fthemes%2Fworldstrides%2Fassets%2Fimages%2Fpin-orange.png\(2.3358607292175293,48.86101631231847\)/2.3358607292175293,48.86009631231847,15/1280x450.png?access_token=pk.eyJ1Ijoid29ybGRzdHJpZGVzIiwiYSI6ImNjZTg3YjM3OTI3MDUzMzlmZmE4NDkxM2FjNjE4YTc1In0.dReWwNs7CEqdpK5AkHkJwg);">
 				<div class="error-content">
-					<h2>But we know where the Louvre is!</h2>
 					<a href="#" class="btn btn-primary">Explore our trips »</a>
 				</div>
 			</section><!-- .about-wrap -->
