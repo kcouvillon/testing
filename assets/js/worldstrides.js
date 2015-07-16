11488b12c50b623e43e5840d1c15b3cd875daef2
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-07-15 21:39:16 -0500

    update itinerary details; refs #116

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -66,7 +66,39 @@
 	
 
  } )( jQuery, window );
+( function( $, window, undefined ) {
+	'use strict';
 
+	// Itineraries
+
+	if ( $( 'body' ).hasClass('single-itinerary') ) {
+
+		$(document).ready(function(){
+
+			$('body')
+				.on('click', '.toggle-dates', function(e){
+
+					if ( $( '.date-list' ).hasClass( 'all-dates' ) ) {
+						
+						$( '.date-range.hidden-dates' ).slideUp();
+						$( '.date-list' ).removeClass( 'all-dates' );
+					
+					} else {
+				
+						$( '.date-range.hidden-dates' ).slideDown();
+						$( '.date-list' ).addClass('all-dates');
+				
+					}
+
+					return false;
+				});
+
+		});
+
+	}
+	
+
+ } )( jQuery );
 ( function( $, window, undefined ) {
 	'use strict';
 
