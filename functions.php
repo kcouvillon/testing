7c044ed15f5ea6a4724e519d1f8f7548902d0f15
Dave Kellam <dave@eightface.com>
2015-07-17 12:49:14 -0400

    Adding backend fields for Divisions #81

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ include WS_PATH . 'includes/class-marketo.php';
 include WS_PATH . 'includes/class-metaboxes.php';
 include WS_PATH . 'includes/class-metaboxes-blocks.php';
 include WS_PATH . 'includes/class-metaboxes-collections.php';
+include WS_PATH . 'includes/class-metaboxes-divisions.php';
 include WS_PATH . 'includes/class-metaboxes-itineraries.php';
 include WS_PATH . 'includes/class-shortcodes.php';
 include WS_PATH . 'includes/class-taxos.php';
