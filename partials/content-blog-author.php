f7a3b8196de4399628fc7e67a01cfb80b1051d94
Dave Kellam <dave@eightface.com>
2015-07-17 23:25:47 -0400

    Add blog author fiels #102

--- partials/content-blog-author.php
+++ partials/content-blog-author.php
@@ -1,10 +1,22 @@
+<?php
+
+if ( class_exists( 'coauthors_plus' ) ) :
+	// Get the Co-Authors for the post
+	$co_authors = get_coauthors();
+
+	// For each Co-Author, echo a wrapper div, their name, and their bio if they have one
+	foreach ( $co_authors as $key => $co_author ) : ?>
 		<div class="entry-author">
-			<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
-			<span>Author Type<br><strong>AUTHOR QUERY VAR</strong></span>
+			<?php // echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+
+			<span>
+				<?php if ( $co_author->author_type ) : ?>
+					<?php echo $co_author->author_type; ?><br>
+				<?php endif; ?>
+
+				<strong><?php echo $co_author->display_name; ?></strong>
+			</span>
 		</div>
 
-		<?php if( is_single() ) : ?>
-
-			
-
-		<?php endif; ?>
+	<?php endforeach; ?>
+<?php endif; ?>
