1d20970ea749797db5152d487d5dd7182f7d50e3
Dave Kellam <dave@eightface.com>
2015-07-17 23:34:31 -0400

    Adding blog author fields; fix #102

--- partials/content-blog-author.php
+++ partials/content-blog-author.php
@@ -7,13 +7,14 @@ if ( class_exists( 'coauthors_plus' ) ) :
 	// For each Co-Author, echo a wrapper div, their name, and their bio if they have one
 	foreach ( $co_authors as $key => $co_author ) : ?>
 		<div class="entry-author">
-			<?php // echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+			<?php if ( has_post_thumbnail( $co_author->ID) ) {
+				echo get_the_post_thumbnail( $co_author->ID, 'thumbnail' );
+			}; ?>
 
 			<span>
 				<?php if ( $co_author->author_type ) : ?>
 					<?php echo $co_author->author_type; ?><br>
 				<?php endif; ?>
-
 				<strong><?php echo $co_author->display_name; ?></strong>
 			</span>
 		</div>
