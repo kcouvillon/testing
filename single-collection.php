2ab94a1ab7d00160cb53035a93a572f4e4f7d7ce
Dave Kellam <dave@eightface.com>
2015-07-20 16:04:23 -0400

    Fixing collection breadcrumb #155

--- single-collection.php
+++ single-collection.php
@@ -32,7 +32,6 @@ get_header(); ?>
 					<nav class="breadcrumbs hide-print">
 						<a href="<?php echo esc_url( home_url( '/explore' ) ); ?>">Explore</a>>
 						<span>Collections</span>>
-						<a href="<?php echo esc_url( home_url( '/collections/' . $term->slug ) ); ?>"><?php echo $term->name; ?></a>>
 						<span><?php the_title(); ?></span>
 					</nav>
 					<h1><?php the_title(); ?></h1>
