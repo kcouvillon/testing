d22403d6b9b11ebfda11da79af7d741342a83b23
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-07-23 18:44:38 -0500

    build out explore tool

---
+++ assets/js/src/explore.js
@@ -0,0 +1,120 @@
+/**
+ * WorldStrides
+ * http://www.worldstrides.com
+ *
+ * Copyright (c) 2015 WorldStrides
+ * Licensed under the GPLv2+ license.
+ */
+ 
+ ( function( $, window, undefined ) {
+	'use strict';
+
+	// Explore
+	// https://mixitup.kunkalabs.com/
+
+	if ( $('body').hasClass('page-template-explore') ) {
+
+		var $exploreResults = $('.explore-results .results');
+
+
+		// On document ready, instantiate .mixItUp()
+		$(document).ready(function(){
+
+			var results = $exploreResults.mixItUp({
+				selectors: {
+					target: '.tile',
+					filter: '.single-filter',
+				},
+				animation: {
+					duration: 350,
+					effects: 'fade stagger(76ms)',
+					easing: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+				}
+			});
+
+		});
+
+
+		// Click events for filters
+		$('.explore-filters')
+			.on('click', '.filter', function(event){
+				event.preventDefault();
+
+				var $this = $(this),
+					filterList = $this.data('filter-list'),
+					slug = $this.attr('href'),
+					text = $this.text(),
+					filters = '';
+
+				if ( ! $this.hasClass('inactive') ) {
+					
+					$this.addClass('inactive');
+					addFilter(filterList, text, slug);
+
+					filters = getCurrentFilters();
+					$exploreResults.mixItUp('filter', filters);
+
+				}
+			})
+			.on('click', '.remove-filter', function(event){
+				event.preventDefault();
+
+				var slug = $(this).parent().data('related'),
+					filters = '';
+
+				removeFilter(slug);
+				
+				filters = getCurrentFilters();
+				$exploreResults.mixItUp('filter', filters);
+
+			})
+			.on('click', '.term-list-toggle', function(event){
+				event.preventDefault();
+
+				var $this = $(this),	
+					target = $this.attr('href'),
+					$filterMenu = $this.parents('.filter-menu');
+
+				$filterMenu.find('.terms-list').addClass('hidden');
+				$(target).removeClass('hidden');
+
+			});
+
+	}
+
+	function addFilter(list, text, slug){
+		$(list).append('<span class="active-filter" data-related="' + slug + '">' + text + '<i class="icon icon-small-close remove-filter"></i></span>');
+	}
+
+	function removeFilter(slug){
+		$('.active-filter[data-related="' + slug + '"]').remove();
+		$('.filter[href="' + slug + '"]').removeClass('inactive');
+	}
+
+	function getCurrentFilters() {
+		var terms = [];
+		$('.current-filters .active-filter').each(function(){
+			var term = $(this).data('related').substr(1);
+			term = '.filter-' + term;
+			terms.push(term);
+		});
+		
+		return terms.join('');
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
--- partials/explore-results.php
+++ partials/explore-results.php
@@ -7,18 +7,43 @@
 		</header>
 		<div class="results clearfix">
 
-			<article ng-repeat="(key, collection) in collections"
-					 class="{{collection.type}} {{collection.slug}} tile tile-third {{ (key > 2) ? 'hidden' : '' }}"
-					 style="background-image:url(http://localhost/worldstrides/wp-content/themes/worldstrides/assets/images/src/patterns/ws_w_pattern{{ (key % 2 == 0) ? '5' : '8' }}.gif);" >
+			<?php
+			global $post; 
+			$count = 0;
+			$collections = get_posts( array('post_type' => 'collection', 'posts_per_page' => 200) );
+			foreach ( $collections as $post ) : setup_postdata($post); ?>
+
+			<?php
+			$count++;
+			if ( has_post_thumbnail() ) {
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', true);
+				$background = $thumb_url_array[0];
+				$scrim = 'linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) ),';
+			} else {
+				$background = get_template_directory_uri().'/assets/images/src/patterns/ws_w_pattern' . (($count % 2 == 0) ? '5' : '8') . '.gif';
+				$scrim = '';
+			}
+			?>
+			<article <?php post_class('tile tile-third'); ?>
+					 style="background-image:<?php echo $scrim . ' url(' . $background . ')'; ?>;" >
 
 				<div class="tile-content collection-content">
 					<ul class="meta collection-meta list-unstyled">
-						<li><a href="#">High School</a></li>
+						<?php 
+						$terms = get_the_terms( get_the_ID(), 'filter' );
+						foreach ( $terms as $term ) : if ( in_array( $term->term_id, get_term_children( 222, 'filter' )) ) { ?>
+
+							<li><?php echo $term->name; ?></li>
+						
+						<?php } endforeach; ?>
 					</ul>
-					<h2 class="tile-title collection-title"><a href="{{collection.link}}">{{ collection.title.rendered }}</a></h2>
+					<h2 class="tile-title collection-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				</div>
 
 			</article>
+
+			<?php endforeach; wp_reset_postdata(); ?>
 
 		</div>
 	</div>
@@ -29,18 +54,53 @@
 		</header>
 		<div class="results clearfix">
 			
-			<article ng-repeat="(key, itinerary) in itineraries"
-					 class="{{itinerary.type}} {{itinerary.slug}} tile tile-third"
-					 style="background-image:url(http://localhost/worldstrides/wp-content/themes/worldstrides/assets/images/src/patterns/ws_w_pattern{{ (key % 2 == 0) ? '1' : '2' }}.gif);" >
+			<?php
+			global $post; 
+			$count = 0;
+			$collections = get_posts( array('post_type' => 'itinerary', 'posts_per_page' => 200) );
+			foreach ( $collections as $post ) : setup_postdata($post); ?>
+
+			<?php
+			$count++;
+			if ( has_post_thumbnail() ) {
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', true);
+				$background = $thumb_url_array[0];
+				$scrim = 'linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) ),';
+			} else {
+				$background = get_template_directory_uri().'/assets/images/src/patterns/ws_w_pattern' . (($count % 2 == 0) ? '1' : '4') . '.gif';
+				$scrim = '';
+			}
+			?>
+			<article <?php post_class('tile tile-third'); ?>
+					 style="background-image:<?php echo $scrim . ' url(' . $background . ')'; ?>;" >
 
 				<div class="tile-content itinerary-content">
 					<ul class="meta itinerary-meta list-unstyled">
-						<li><a href="#">High School</a></li>
+						<?php 
+						// $terms = get_the_terms( get_the_ID(), 'filter' );
+						// $top_level_terms = array();
+
+						// foreach ( $terms as $term ) {
+						// 	$ancestors = get_ancestors( $term->term_id, 'filter' );
+						// 	if ( in_array( 11, $ancestors) && ! in_array( $ancestors[0], $top_level_terms ) ) {
+						// 		array_push( $top_level_terms, $ancestors[0] );
+						// 	}
+						// }
+
+						// foreach ( $top_level_terms as $term_id ) {
+						// 	$top_term = get_term( $term_id, 'filter' );
+						// 	echo '<li>' . $top_term->name . '</li>';
+						// }
+
+						?>
 					</ul>
-					<h2 class="tile-title itinerary-title"><a href="{{itinerary.link}}">{{ itinerary.title.rendered }}</a></h2>
+					<h2 class="tile-title itinerary-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				</div>
 
 			</article>
+
+			<?php endforeach; wp_reset_postdata(); ?>
 
 		</div>
 	</div>
