e1abd883cbbe20020c464331780aa3028ffd4334
Dave Kellam <dave@eightface.com>
2015-07-24 15:56:25 -0400

    Wire up request info box on collections #174

--- single-collection.php
+++ single-collection.php
@@ -325,13 +325,15 @@ get_header(); ?>
 		<?php if ( 'on' == $additional_info_request_box ) : ?>
 			<div class="blog-single-cta">
 				<span class="h2">Request Information about a WorldStrides Program</span>
-				<span>I am a</span>
-				<select name="" id="">
-					<option value="/request-info/?type=parent">Parent</option>
-					<option value="/request-info/?type=traveler">Traveler</option>
-					<option value="/request-info/?type=teacher">Teacher</option>
-				</select>
-				<button class="btn btn-primary">Get the Info</button>
+				<form>
+					<span>I am a</span>
+					<select id="selectMenu">
+						<option value="/request-info/?type=parent">Parent</option>
+						<option value="/request-info/?type=traveler">Traveler</option>
+						<option value="/request-info/?type=teacher">Teacher</option>
+					</select>
+					<input type="submit" class="btn btn-primary" value="Get the Info" onclick="window.open(selectMenu.options[selectMenu.selectedIndex].value)">
+				</form>
 			</div>
 		<?php endif; ?>
 
