ce1a3a29b48d41ce1a7626af8c0f6f7071c40f3a
Dave Kellam <dave@eightface.com>
2015-07-27 19:34:16 -0400

    Grunty grunt

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-07-24
+/*! WorldStrides - v0.1.0 - 2015-07-27
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
