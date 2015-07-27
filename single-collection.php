24a663160fe6ce37e81392b5bc3e3e953357b19d
Dave Kellam <dave@eightface.com>
2015-07-27 15:11:13 -0400

    Adding request info partial

--- single-collection.php
+++ single-collection.php
@@ -317,18 +317,7 @@ get_header(); ?>
 		?>
 
 		<?php if ( 'on' == $additional_info_request_box ) : ?>
-			<div class="blog-single-cta">
-				<span class="h2">Request Information about a WorldStrides Program</span>
-				<form>
-					<span>I am a</span>
-					<select id="selectMenu">
-						<option value="/request-info/?type=parent">Parent</option>
-						<option value="/request-info/?type=traveler">Traveler</option>
-						<option value="/request-info/?type=teacher">Teacher</option>
-					</select>
-					<input type="submit" class="btn btn-primary" value="Get the Info" onclick="window.open(selectMenu.options[selectMenu.selectedIndex].value)">
-				</form>
-			</div>
+			<?php get_template_part( 'partials/request-info' ); ?>
 		<?php endif; ?>
 
 		<?php if ( $additional_info_text && $additional_info_email_title && $additional_info_email_text ) : ?>
