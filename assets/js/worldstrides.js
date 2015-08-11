4cd4bbffd23147cac81cb8ea535a13632b94b030
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-08-11 17:37:06 +0700

    #29: continue to build out pricing module

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-08-10
+/*! WorldStrides - v0.1.0 - 2015-08-11
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
