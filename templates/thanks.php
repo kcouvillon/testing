3572fa1730118c8f288fdc7330932ca10e5dce87
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-09-01 15:11:38 -0400

    style thanks

--- templates/thanks.php
+++ templates/thanks.php
@@ -5,17 +5,18 @@
 * Google Analytics goal page for all thank-yous.  This page receives variables as POST and forwards them to Marketo
 */
 ?>
-<?php // get_header(); // TODO: reinstate this once it's wired up properly ?>
+<?php get_header(); ?>
 
 <!-- Pull in Munchkin by hand. TODO: pull this in dynamically. -->
 <script type="text/javascript" src="//munchkin.marketo.net//munchkin.js"></script>
-
-<h1>Thanks for your Interest in WorldStrides</h1>
-<p>&nbsp;</p>
-<p>POST VARIABLES RECEIVED:</p>
-<pre>
-<?php 
-	print_r($_POST);
-?>
-</pre>
-<?php // get_footer(); // TODO: reinstate this once it's wired up properly ?>
+<div class="ws-container">
+	<h1>Thanks for your Interest in WorldStrides</h1>
+	<p>&nbsp;</p>
+	<p>POST VARIABLES RECEIVED:</p>
+	<pre>
+	<?php 
+		print_r($_POST);
+	?>
+	</pre>
+</div>
+<?php get_footer(); ?>
