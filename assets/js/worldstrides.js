0f25844f388e3afff8c54b15b2a2d4d57bc49fac
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-09-01 09:06:38 +0700

    #234: fix block text alignment

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-08-31
+/*! WorldStrides - v0.1.0 - 2015-09-01
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
