6270d3dd78a1f46e683399cfdee9f7ed514b5372
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-09-01 11:49:52 -0400

    debugging the thanks page some more

--- universal-thanks-page.php
+++ universal-thanks-page.php
@@ -5,7 +5,7 @@
 * Google Analytics goal page for all thank-yous.  This page receives variables as POST and forwards them to Marketo
 */
 ?>
-<?php get_header(); ?>
+<?php // get_header(); // TODO: reinstate this once it's wired up properly ?>
 <h1>Thanks for your Interest in WorldStrides</h1>
 <p>&nbsp;</p>
 <p>POST VARIABLES RECEIVED:</p>
@@ -14,4 +14,4 @@
 	print_r($_POST);
 ?>
 </pre>
-<?php get_footer(); ?>
+<?php // get_footer(); // TODO: reinstate this once it's wired up properly ?>
