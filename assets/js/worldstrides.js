3c5267ab8395cc0aee80ecaefb734844684dae9d
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-09-14 16:58:00 +0700

    make footer menus into drawers #283

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -585,6 +585,40 @@
 ( function( $, window, undefined ) {
 	'use strict';
 
+	// General
+
+	$('#colophon')
+		.on('click', '.menu > li > a .icon', function(event){
+			event.preventDefault();
+			console.log(event);
+
+			var $this = $(this),
+				$target = $this.parent().siblings('.sub-menu');
+
+			if ( $this.hasClass('active') || $target.hasClass('open') ) {
+				$target
+					.slideUp()
+					.removeClass('open');
+				$this
+					.removeClass('active');
+			} else {
+				$target
+					.slideDown()
+					.addClass('open');
+				$this
+					.addClass('active');
+			}
+		});
+
+
+
+
+
+})(jQuery);
+
+( function( $, window, undefined ) {
+	'use strict';
+
 	// MARKETO FORM BEHAVIOR
 
 	$(document).ready(function() {
