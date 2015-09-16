693a8d3412ddcdd759215ee78bfb6ce9cfe411c3
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-09-16 13:51:30 +0700

    update get info form with labels #27

--- partials/form-universal.php
+++ partials/form-universal.php
@@ -4,7 +4,7 @@
 		<h2 class="form-title">Ready to Learn More About Traveling with WorldStrides?</h2>
 		<ul class="form-fields list-unstyled">
 			<li class="field">
-				My role is
+				<label for="get-info-Title">My role is</label>
 				<select id="get-info-Title" name="mkto_Title" title="Role">
 					<option value="">Select...</option>
 					<option value="stu">Student</option>
@@ -16,7 +16,7 @@
 				</select>
 			</li>
 			<li class="field">
-				I am interested in
+				<label for="get-info-wsProduct">I am interested in</label>
 				<select id="get-info-wsProduct" name="mkto_wsProduct" title="General Interest">
 					<option value="Unknown">Select...</option>
 					<option value='Middle School - History' class='par ele mse hse'>U.S. History Themed Tours</option>
@@ -29,7 +29,7 @@
 				</select>
 			</li>
 			<li id="moremusicfield" class="field" style="display:none;">
-				I want to learn more about
+				<label for="get-info-moremusic">I want to learn more about</label>
 				<select id="get-info-moremusic" name="mkto_moremusic" title="Musical Interest">
 					<option value="">Select...</option>
 					<option value="Music Festivals">Music Festivals </option>
@@ -44,7 +44,7 @@
 			</li>
 
 			<li id="get-info-tour-scheduled" name="mkto_areyouCurrentlyScheduledforaWorldStridestrip" class="field" title="I have a Tour Scheduled">
-				I have a tour scheduled:
+				<label>I have a tour scheduled:</label>
 				&nbsp;&nbsp;
 				<input type="radio" name="tour" id="tour-yes" value="yes" title="Yes">
 				<label for="tour-yes">Yes</label>
@@ -58,34 +58,42 @@
 		<ul class="form-fields list-unstyled">
 			<li class="field field-complex">
 				<div class="field-left">
+					<label for="get-info-first-name" class="block no-placeholder">First Name</label>
 					<input id="get-info-first-name" type="text" name="mkto_FirstName" value="" placeholder="First Name" title="First Name">
 				</div>
 				<div class="field-right">
+					<label for="get-info-last-name" class="block no-placeholder">Last Name</label>
 					<input id="get-info-last-name" type="text" name="mkto_LastName" value="" placeholder="Last Name" title="Last Name">
 				</div>
 			</li>
 			<li class="field field-complex">
 				<div class="field-left">
+					<label for="get-info-email" class="block no-placeholder">Email Address</label>	
 					<input id="get-info-email" type="email" name="mkto_Email" value="" placeholder="Email Address" title="Email Address">
 				</div>
 				<div class="field-right">
+					<label for="get-info-phone" class="block no-placeholder">Phone Number</label>
 					<input id="get-info-phone" type="tel" name="mkto_Phone" value="" placeholder="Phone Number" title="Preferred Phone Number">
 				</div>
 			</li>
 			<li class="field field-complex">
 				<div class="field-left">
+					<label for="get-info-state" class="block no-placeholder">Select State...</label>
 					<select id="get-info-state" name="mkto_companyState" title="School State">
 						<option value="">Select State...</option>
 					</select>
 				</div>
 				<div class="field-right">
+					<label for="get-info-city" class="block no-placeholder">School City</label>
 					<input id="get-info-city" type="text" name="mkto_companyCity" value="" placeholder="School City" title="School City">
 				</div>
 			</li>
 			<li class="field">
+				<label for="get-info-school" class="block no-placeholder">School Name</label>
 				<input id="get-info-school" type="text" name="mkto_Company" value="" placeholder="School Name" title="School Name">
 			</li>
 			<li class="field">
+				<label for="get-info-comment" class="block no-placeholder">Comments or Questions?</label>
 				<textarea id="get-info-comment" name="mkto_formcomments" rows="3" cols="30" style="max-height: none;" placeholder="Comments or Questions?" title="Comments or Questions"></textarea>
 			</li>
 		</ul>
