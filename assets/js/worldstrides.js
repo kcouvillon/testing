4bf92c7427a6fcd8ed5730c68253ace28525c85d
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-09-28 13:35:58 -0400

    grunt

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-09-25
+/*! WorldStrides - v0.1.0 - 2015-09-28
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
