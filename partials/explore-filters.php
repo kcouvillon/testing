1f30dfbacc9fb9fddb97fab59891c93b8518c621
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-10-01 09:09:21 +0700

    add faith-based icon and update explore filters #307

--- partials/explore-filters.php
+++ partials/explore-filters.php
@@ -71,14 +71,14 @@ $continents = get_terms( 'filter', $continentsArgs );
 
 						<li>
 							<a href="#faith-based" class="filter" data-filter-list=".interests-filters">
-								<i class="icon icon-faith-and-service"></i>
+								<i class="icon icon-faith-based"></i>
 								Faith-based
 							</a>
 						</li>
 
 						<li>
 							<a href="#service" class="filter" data-filter-list=".interests-filters">
-								<i class="icon icon-faith-and-service"></i>
+								<i class="icon icon-service"></i>
 								Service
 							</a>
 						</li>
