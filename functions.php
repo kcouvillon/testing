61b1df092f9e4df1dba250a7c4a3a55de3ca985e
Dave Kellam <dave@eightface.com>
2015-10-07 15:51:57 -0400

    Adding before/after blocks to resource targets
    Move resource target metaboxes to its own file. Probably ended up being unnecessary, as I wasn't able to get before/after blocks working with the section repeater fields. So, we're limited to one set of before and after blocks.
    
    #305

--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ include WS_PATH . 'includes/class-metaboxes-collections.php';
 include WS_PATH . 'includes/class-metaboxes-divisions.php';
 include WS_PATH . 'includes/class-metaboxes-itineraries.php';
 include WS_PATH . 'includes/class-metaboxes-filter-endpoints.php';
+include WS_PATH . 'includes/class-metaboxes-resource-targets.php';
 include WS_PATH . 'includes/class-shortcodes.php';
 include WS_PATH . 'includes/class-taxos.php';
 
