49e85941c713e79dc357e5c2d13cb1af810b67ad
Dave Kellam <dave@eightface.com>
2015-10-07 17:12:36 -0400

    Bringing in header changes from itineraries
    Also removed some unneeded sections
    
    #286

--- single-custom-page.php
+++ single-custom-page.php
@@ -28,37 +28,37 @@ get_header(); ?>
 			} ?>
 			<section class="primary-section">
 				<header class="section-header<?php echo $class; ?>" style="background-image: <?php echo $background; ?>;">
-					<div class="section-header-content">
 
-						<nav class="breadcrumbs hide-print">
-							<a href="<?php echo esc_url( home_url( '/explore/' ) ); ?>">Explore</a>
-							<span>Collections</span>
-							<?php if ( ! empty( $term ) ) : ?>
-								<a href="<?php echo esc_url( home_url( '/collections/' . $term->slug . '/' ) ); ?>"><?php echo $term->name; ?></a>>
+					<div class="mobile-hero">
+						<?php the_post_thumbnail( 'large' ); ?>
+					</div>
+
+					<div class="ws-container">
+						<div class="section-header-content">
+
+							<nav class="breadcrumbs hide-print">
+								`							<a href="<?php echo esc_url( home_url( '/explore/' ) ); ?>">Explore Educational Travel</a>>
+								<?php if( ! empty( $term ) ) : ?>
+									<a href="<?php echo esc_url( home_url( '/collections/' . $term->slug . '/' ) ); ?>"><?php echo $term->name; ?></a>>
+								<?php endif; ?>
+								<span><?php the_title(); ?></span>
+							</nav>
+							<span class="print-only"><?php the_permalink(); ?></span>
+							<h1><?php the_title(); ?></h1>
+
+							<?php $subtitle = get_post_meta( $post->ID, 'itinerary_subtitle', true ); ?>
+
+							<?php if ( $subtitle ) : ?>
+								<p class="header-subtitle"><?php echo apply_filters( 'the_title', $subtitle ); ?></p>
 							<?php endif; ?>
-							<span><?php the_title(); ?></span>
-						</nav>
-						<span class="print-only"><?php the_permalink(); ?></span>
 
-						<h1><?php the_title(); ?></h1>
+							<?php the_content(); ?>
 
-						<?php $subtitle = get_post_meta( $post->ID, 'itinerary_subtitle', true ); ?>
+							<?php if ( $phone ) : ?>
+								<p class="print-only">Call for more information: <?php echo $phone; ?></p>
+							<?php endif; ?>
+						</div>
 
-						<?php if ( $subtitle ) : ?>
-							<p class="header-subtitle"><?php echo apply_filters( 'the_title', $subtitle ); ?></p>
-						<?php endif; ?>
-
-						<?php the_content(); ?>
-
-						<?php if ( 'smithsonian' == $itinerary_type ) : ?>
-							<h3 class="hide-print">
-								<img class="smithsonian" alt="smithsonian" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/smithsonian.png' ); ?>" />
-							</h3>
-						<?php endif; ?>
-
-						<?php if ( $phone ) : ?>
-							<p class="print-only">Call for more information: <?php echo $phone; ?></p>
-						<?php endif; ?>
 					</div>
 
 					<?php get_template_part( 'partials/content', 'tooltips' ); ?>
@@ -76,41 +76,6 @@ get_header(); ?>
 						if ( ! empty( $highlights[1]['image'] ) ) : ?>
 							<li><a href="#section-<?php echo $section_link;
 								$section_link ++; ?>">Highlights</a></li>
-						<?php endif; ?>
-
-						<?php
-						// Get Before Blocks
-						$before_block_sections = get_post_meta( $post->ID, 'itinerary_blocks_before_list', true ); ?>
-
-						<?php if ( ! empty( $before_block_sections ) ) : ?>
-							<?php foreach ( $before_block_sections as $section ) : ?>
-								<?php if ( ! empty ( $section['title'] ) ) : ?>
-									<li><a href="#section-<?php echo $section_link;
-										$section_link ++; ?>"><?php echo $section['title']; ?></a></li>
-								<?php endif; ?>
-							<?php endforeach; ?>
-						<?php endif; ?>
-
-						<?php
-						// Get Itinerary
-						$itinerary = get_post_meta( $post->ID, 'itinerary_days_list', true ); ?>
-
-						<?php if ( ! empty( $itinerary ) ) : ?>
-							<li><a href="#section-<?php echo $section_link;
-								$section_link ++; ?>">Itinerary</a></li>
-						<?php endif; ?>
-
-						<?php
-						// Get After Blocks
-						$after_block_sections = get_post_meta( $post->ID, 'itinerary_blocks_after_list', true ); ?>
-
-						<?php if ( ! empty( $after_block_sections ) ) : ?>
-							<?php foreach ( $after_block_sections as $section ) : ?>
-								<?php if ( ! empty ( $section['title'] ) ) : ?>
-									<li><a href="#section-<?php echo $section_link;
-										$section_link ++; ?>"><?php echo $section['title']; ?></a></li>
-								<?php endif; ?>
-							<?php endforeach; ?>
 						<?php endif; ?>
 
 					</ul>
@@ -231,31 +196,6 @@ get_header(); ?>
 
 				</div>
 
-			</section>
-
-			<section class="tour-sharing hide-print">
-				<?php $pdf = get_post_meta( $post->ID, 'itinerary_pdf' ); ?>
-				<ul class="sharing-links list-unstyled">
-					<li>
-						<a href="<?php echo 'mailto:?subject=' . rawurlencode( get_the_title() ) . '&body=' . urlencode( get_the_permalink() ); ?>"><i class="icon icon-email"></i> Email Itinerary</a>
-					</li>
-					<li><a href="javascript:window.print()"><i class="icon icon-print"></i> Print Itinerary</a></li>
-					<?php if ( $pdf ) : ?>
-						<li><a href="<?php echo $pdf[0]; ?>"><i class="icon icon-pdf"></i> Download PDF</a></li>
-					<?php endif; ?>
-					<li>
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_the_permalink() ); ?>&t=<?php echo urlencode( get_the_title() ); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=no,scrollbars=yes,height=300,width=600');return false;" target="_blank" title="Share on Facebook">
-							<i class="icon-facebook"></i> Share on Facebook
-						</a>
-					</li>
-					<li>
-						<a href="https://twitter.com/share?url=<?php echo urlencode( get_the_permalink() ); ?>&via=worldstrides&text=<?php echo urlencode( get_the_title() ); ?>"
-						   onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"
-						   target="_blank" title="Share on Twitter">
-							<i class="icon-twitter"></i> Share on Twitter
-						</a>
-					</li>
-				</ul>
 			</section>
 
 			<?php $section_num = 1; // set first section number ?>
