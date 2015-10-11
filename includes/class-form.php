5706a66d1ab223b46c20e9c0979f8f9bea6037b0
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-10-11 17:04:38 -0400

    bug fix for current_page_url() HTTPS var

--- includes/class-form.php
+++ includes/class-form.php
@@ -144,7 +144,7 @@ class WS_Form {
 	 */
 	public static function current_page_url() {
 		$pageURL = 'http';
-		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+		if (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
 		$pageURL .= "://";
 		if ($_SERVER["SERVER_PORT"] != "80") {
 			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
