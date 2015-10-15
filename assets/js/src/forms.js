7454c95e77d9f18b78495cced6558a0ea66b4a93
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-10-15 16:53:06 -0400

    more verbose errors in console for form

--- assets/js/src/forms.js
+++ assets/js/src/forms.js
@@ -533,6 +533,7 @@
 			 success : function( responseText ) {
 				 // form.html('Your request has been submitted successfully - ' + responseText );
 				 form.html('<h2>Thanks for your interest in traveling with WorldStrides! One of our representatives will reach out to you very shortly. In the meantime, please feel free to continue <a href="/explore/">exploring</a> all the destinations on our site.</h2>');
+				 console.log(responseText);
 			 }
 		 };
 
