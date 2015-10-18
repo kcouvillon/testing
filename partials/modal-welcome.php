9abc300c4bc988aad2fbd4457092122d3d00d0e4
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-10-18 15:52:22 -0400

    basic modal in place, needs styling and text

---
+++ partials/modal-welcome.php
@@ -0,0 +1,33 @@
+<?php 
+ /**
+  * Modal window that appears once to welcome users -- checks a cookie to avoid reappearing.
+  */
+
+if( !isset( $ws_welcome_cookie ) && !isset( $_COOKIE['ws_welcome_cookie'] ) ) : ?>
+
+<div id="welcome-modal" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		jQuery('#welcome-modal').modal('toggle');
+	});
+</script>
+
+ <?php
+endif;
