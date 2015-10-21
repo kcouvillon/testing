33119e7bef317847c8ba35af8759b88d200e179a
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-10-21 14:10:46 -0400

    register page mods

---
+++ assets/js/src/register.js
@@ -0,0 +1,17 @@
+/**
+ * WorldStrides
+ * http://www.worldstrides.com
+ *
+ * Copyright (c) 2015 WorldStrides
+ * Licensed under the GPLv2+ license.
+ */
+ 
+ ( function( jQuery) {
+	'use strict';
+
+	// for the /register/ page
+	jQuery('#have-username-label').click( function() {
+		
+	})
+
+ } )( jQuery );	
