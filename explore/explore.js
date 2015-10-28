585f2175f0ddf3f0fdc111d089fb9dfb751adbdb
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-10-28 11:23:33 +0700

    more work on explore interface

--- explore/explore.js
+++ explore/explore.js
@@ -4,12 +4,12 @@ exploreApp.config([ '$routeProvider', function($routeProvider){
 
 	$routeProvider
 		.when('/featured', {
-			templateUrl: wsExplore.path + '/views/featured.html',
+			templateUrl: wsTheme.explore + '/views/featured.html',
 			controller: 'ExploreController',
 			controllerAs: 'ctrl'
 		})
 		.when('/:filters', {
-			templateUrl: wsExplore.path + '/views/filters.html',
+			templateUrl: wsTheme.explore + '/views/filters.html',
 			controller: 'ExploreController',
 			controllerAs: 'ctrl'
 		})
@@ -26,14 +26,31 @@ exploreApp.service('Terms', function($q, $http){
 
 	_this.getAll = function( taxonomy ){
 		return $q(function(resolve, reject){
-			$http.get("http://ws.local/wp-json/wp/v2/terms/" + taxonomy)
+			$http.get(wsTheme.explore + "/" + taxonomy + ".json")
 				.then(function(response){
 					resolve(response);
 				}, function(error){
 					reject(error);
 				});
 		});
-	}
+	};
+
+	_this.getChildrenOf = function( parent, data ){
+		if ( !parent || !data )
+			return false;
+
+		var children = [],
+			parentType = ( typeof parent == 'string' ) ? 'slug' : 'ID';
+
+		angular.forEach(data, function(value, key){
+			if ( value.parent && value.parent[parentType] == parent ) {
+				if ( value.ID !== 384 ) // extra check to exclude faith-based & service
+					children.push(value);
+			}
+		});
+
+		return children;
+	};
 
 });
 
@@ -43,7 +60,7 @@ exploreApp.service('Itineraries', function($q, $http){
 
 	_this.getAll = function(){
 		return $q(function(resolve, reject){
-			$http.get("http://ws.local/wp-json/wp/v2/posts")
+			$http.get(wsTheme.explore + "/itineraries.json")
 				.then(function(response){
 					resolve(response);
 				}, function(error){
@@ -54,20 +71,32 @@ exploreApp.service('Itineraries', function($q, $http){
 
 });
 
-var ExploreController = function(Terms){
+var ExploreController = function(Terms, Itineraries){
 
 	var _this = this;
 
 	_this.loading = true;
+	_this.wsTheme = wsTheme;
+	_this.continents;
+	_this.travelers;
+	_this.interests;
+	_this.itineraries;
 
-	Terms.getAll('filter').then(function(response){
-		console.log(response);
+	Itineraries.getAll().then(function(response){
+		_this.itineraries = response.data;
+	}, function(error){
+		console.log(error);
+	});
+
+	Terms.getAll('filters').then(function(response){
+		_this.continents = Terms.getChildrenOf('destination', response.data);
+		_this.travelers = Terms.getChildrenOf('traveler', response.data);
+		_this.interests = Terms.getChildrenOf('interest', response.data);
 		_this.loading = false;
 	}, function(error){
 		console.log(error);
-		_this.loading = false;
 	});
 
 };
-ExploreController.$inject = ['Terms'];
+ExploreController.$inject = ['Terms', 'Itineraries'];
 exploreApp.controller('ExploreController', ExploreController);
