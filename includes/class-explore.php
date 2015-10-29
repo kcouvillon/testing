5ee2d9d09db3e2058dbbd6d21182cb5c7ceecf08
Dave Kellam <dave@eightface.com>
2015-10-29 00:45:50 -0400

    Basic for a custom api for explore tool
    Realizing the WP JSON API is probably overkill for our needs. Was also returning very large json results. This should trim it down to the essentials.
    
    Just the working basics for filter results at the moment.
    
    #354

--- includes/class-explore.php
+++ includes/class-explore.php
@@ -34,14 +34,13 @@ class WS_Explore {
 	 * Sets up actions and filters.
 	 */
 	protected function _init() {
-		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+		add_action( 'init', array( $this, 'explore_endpoints' ) );
+		add_action( 'template_redirect', array( $this, 'filters' ) );
 	}
 
-	public function register_routes() {
-		register_rest_route( 'worldstrides/v1', '/filter/(?P<slug>\d+)', array(
-			'methods' => 'GET',
-			'callback' => array( $this, 'filters' )
-		) );
+	public function explore_endpoints() {
+		add_rewrite_tag( '%api-filters%', '([^&]+)' );
+		add_rewrite_rule( 'ws-api/v1/explore/results/([^&]+)/?', 'index.php?api-filters=$matches[1]', 'top' );
 	}
 
 	/**
@@ -49,11 +48,20 @@ class WS_Explore {
 	 */
 	public static function filters() {
 
-		$filters = array( 'high-school', 'china' );
+		global $wp_query;
 
-		$itinerary_args = array(
+		$filters = $wp_query->get( 'api-filters' );
+		$filters = explode( ',', $filters );
+
+		if ( empty( $filters ) ) {
+			return;
+		}
+
+		$filter_data = array();
+
+		$args = array(
 			'post_type' => 'itinerary',
-			'posts_per_page' => 500, // very large, but better than -1
+			'posts_per_page' => 200, // very large, but better than -1
 			'tax_query' => array(
 				array(
 					'taxonomy' => 'filter',
@@ -67,12 +75,18 @@ class WS_Explore {
 			'order' => 'ASC'
 		);
 
-		$itineraries = new WP_Query( $itinerary_args );
+		$query_data = new WP_Query( $args );
 
-		// echo '<h2>' . $itineraries->post_count . '</h2>';
-		// var_dump( $itineraries );
+		if ( $query_data->have_posts() ) : while ( $query_data->have_posts() ) : $query_data->the_post();
+			$img_id = get_post_thumbnail_id();
+			$img = wp_get_attachment_image_src( $img_id, 'medium' );
+			$filter_data[] = array(
+				'link'  => esc_url( $img[0] ),
+				'title' => get_the_title(),
+			);
+		endwhile; wp_reset_postdata(); endif;
 
-		return $itineraries;
+		wp_send_json( $filter_data );
 	}
 }
 
