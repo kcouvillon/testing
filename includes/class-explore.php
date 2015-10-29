a18058ba132a14ea654ebc245b18443b4f421d84
Dave Kellam <dave@eightface.com>
2015-10-29 15:38:53 -0400

    Add json endpoint for explore featured
    endpoint: /ws-api/v1/explore/data/featured/
    
    #354

--- includes/class-explore.php
+++ includes/class-explore.php
@@ -35,25 +35,28 @@ class WS_Explore {
 	 */
 	protected function _init() {
 		add_action( 'init', array( $this, 'explore_endpoints' ) );
-		add_action( 'template_redirect', array( $this, 'filter_requests' ) );
+		add_action( 'template_redirect', array( $this, 'filter_results' ) );
+		add_action( 'template_redirect', array( $this, 'api_data_handler' ) );
 	}
 
 	/**
 	 * Create endpoints for ws-api calls
 	 */
 	public function explore_endpoints() {
-		add_rewrite_tag( '%api-filters%', '([^&]+)' );
-		add_rewrite_rule( 'ws-api/v1/explore/results/([^&]+)/?', 'index.php?api-filters=$matches[1]', 'top' );
+		add_rewrite_tag( '%api-filter-results%', '([^&]+)' );
+		add_rewrite_tag( '%api-data%', '([^&]+)' );
+		add_rewrite_rule( 'ws-api/v1/explore/results/([^&]+)/?', 'index.php?api-filter-results=$matches[1]', 'top' );
+		add_rewrite_rule( 'ws-api/v1/explore/data/([^&]+)/?', 'index.php?api-data=$matches[1]', 'top' );
 	}
 
 	/**
 	 * Provide a json array of Itineraries and Collections for a ws-api endpoint request
 	 */
-	public static function filter_requests() {
+	public static function filter_results() {
 
 		global $wp_query;
 
-		$filters = $wp_query->get( 'api-filters' );
+		$filters = $wp_query->get( 'api-filter-results' );
 		$filters = explode( ',', $filters );
 
 		if ( empty( $filters ) ) {
@@ -118,6 +121,85 @@ class WS_Explore {
 		} else {
 			// This messed up the whole site...
 			// wp_send_json_error( $filter_data );
+
+		}
+
+		return;
+	}
+
+	/**
+	 * Provide a json array of Itineraries and Collections for a ws-api endpoint request
+	 */
+	public static function api_data_handler() {
+
+		global $wp_query;
+
+		$api_data = $wp_query->get( 'api-data' );
+		//echo "1" . $api_data; exit();
+
+		if ( 'featured' === $api_data ) {
+			WS_Explore::explore_featured();
+		}
+	}
+	/**
+	 * Provide a json array of Itineraries and Collections for a ws-api endpoint request
+	 */
+	public static function explore_featured() {
+
+		$explore_id = 47;
+
+		$select_itineraries_ids = get_post_meta( $explore_id, 'explore_attached_itineraries', true );
+		$select_collections_ids = get_post_meta( $explore_id, 'explore_attached_collections', true );
+
+		$query_ids = array_merge( $select_itineraries_ids, $select_collections_ids );
+
+		$args = array(
+			'post_type'      => array( 'itinerary', 'collection' ),
+			'posts_per_page' => 100,
+			'no_found_rows'          => true,
+			'post__in' => $query_ids,
+			'orderby' => 'post__in'
+		);
+
+		$data = array();
+		$query_data = new WP_Query( $args );
+
+		if ( $query_data->have_posts() ) {
+			while ( $query_data->have_posts() ) {
+				$query_data->the_post();
+
+				$img_id    = get_post_thumbnail_id();
+				$img       = wp_get_attachment_image_src( $img_id, 'large' );
+				$post_type = get_post_type();
+				$terms     = get_the_terms( get_the_ID(), 'filter' );
+				$term_list = array();
+				$meta_list = array();
+
+				if ( $terms ) {
+					foreach ( $terms as $term ) {
+						array_push( $term_list, array(
+							'name' => $term->name,
+							'slug' => $term->slug
+						) );
+
+						if ( in_array( $term->term_id, get_term_children( 222, 'filter' ) ) ) {
+							array_push( $meta_list, array( 'name' => $term->name ) );
+						}
+					}
+				}
+
+				$data[ $post_type ][] = array(
+					'title'          => get_the_title(),
+					'featured_image' => esc_url( $img[0] ),
+					'link'           => get_the_permalink(),
+					'filter'         => $term_list,
+					'meta'           => $meta_list
+
+				);
+			}
+			wp_reset_postdata();
+
+			wp_send_json_success( $data );
 
 		}
 
