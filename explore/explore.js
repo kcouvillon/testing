38509bd3367df286667fbb528187b6c3cbdef70a
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-10-30 07:22:34 +0700

    Angular Explore setup function to auto create urls for terms #354

--- explore/explore.js
+++ explore/explore.js
@@ -8,11 +8,6 @@ exploreApp.config([ '$routeProvider', function($routeProvider){
 			controller: 'ExploreController',
 			controllerAs: 'ctrl'
 		})
-		// .when('/:filters', {
-		// 	templateUrl: WS.explore + '/views/results.html',
-		// 	controller: 'ExploreController',
-		// 	controllerAs: 'ctrl'
-		// })
 		.when('/:travelers/:interests/:destinations', {
 			templateUrl: WS.explore + '/views/results.html',
 			controller: 'ExploreController',
@@ -21,7 +16,6 @@ exploreApp.config([ '$routeProvider', function($routeProvider){
 		.otherwise({
 			redirectTo: '/featured'
 		});
-
 
 }]);
 
@@ -82,11 +76,62 @@ exploreApp.service('Posts', function($q, $http){
 
 });
 
+exploreApp.directive('termHref', function($route) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+
+   		function getUrl( slug, filterGroup ) {
+			var params = angular.copy($route.current.params),
+				keys = Object.keys(params),
+				url = '';
+			
+			if ( Object.keys(params).length > 0 ) {
+				if ( params[filterGroup].indexOf('all-') > -1 ) {
+					params[filterGroup] = slug;
+				} else if ( params[filterGroup].indexOf(slug) > -1 ) {
+					params[filterGroup] = params[filterGroup];
+				} else {
+					params[filterGroup] += ',' + slug;
+				}
+				url = '#/' + params[keys[0]] +'/'+ params[keys[1]] +'/'+ params[keys[2]];	
+			} else {
+				params = {
+					travelers: 'all-travelers',
+					interests: 'all-interests',
+					destinations: 'all-destinations'
+				};
+				params[filterGroup] = slug;
+				url = '#/' + params.travelers +'/'+ params.interests +'/'+ params.destinations;
+			}
+
+			return url;
+		}
+
+		var hrefData = attrs.termHref.split(','),
+			url = getUrl( hrefData[0], hrefData[1] );
+		element.attr('href', url);
+    }
+  }
+});
+
 var ExploreController = function(Terms, Posts, $route){
 
 	var _this = this,
 		route = $route.current.params,
 		query;
+
+	_this.loading = true;
+	_this.hasFilters = (Object.keys(route).length > 0) ? true : false;
+	_this.WS = WS;
+	_this.$route = $route;
+	_this.travelers;
+	_this.interests;
+	_this.continents;
+	_this.itineraries;
+	_this.collections;
+	_this.showInterestsList = 'interests-parent';
+	_this.showDestinationsList = 'destinations-parent';
 
 	if ( Object.keys(route).length > 0 ) {
 		query = [];
@@ -98,18 +143,6 @@ var ExploreController = function(Terms, Posts, $route){
 	} else {
 		query = 'featured';
 	}
-
-	console.log(query);
-
-	_this.loading = true;
-	_this.WS = WS;
-	_this.travelers;
-	_this.interests;
-	_this.continents;
-	_this.itineraries;
-	_this.collections;
-	_this.showInterestsList = 'interests-parent';
-	_this.showDestinationsList = 'destinations-parent';
 
 	Posts.get(query).then(function(response){
 		_this.itineraries = response.data.data.itinerary;
@@ -127,6 +160,8 @@ var ExploreController = function(Terms, Posts, $route){
 		console.log(error);
 	});
 
+	// console.log(_this.formatUrl('china', 'destinations'));
+
 };
 
 ExploreController.prototype.showTermList = function( list, term ){
@@ -135,7 +170,7 @@ ExploreController.prototype.showTermList = function( list, term ){
 	} else if ( list == 'destination' ) {
 		this.showDestinationsList = term;
 	}
-}
+};
 
 ExploreController.prototype.toggleFilterMenu = function( menu ) {
 	var menu = angular.element(menu);
@@ -144,8 +179,7 @@ ExploreController.prototype.toggleFilterMenu = function( menu ) {
 	} else {
 		menu.addClass('closed');
 	}
-}
-
+};
 
 
 
