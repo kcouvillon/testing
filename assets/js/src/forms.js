2283c83ac28fa2c674d9353504ae8e9407c50b93
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-11-09 14:07:09 -0500

    more POST passing JS fixes

--- assets/js/src/forms.js
+++ assets/js/src/forms.js
@@ -139,6 +139,7 @@
 		if( undefined !== wsData.passedInRole ){
 			if( jQuery('select#get-info-Title option:contains("' + wsData.passedInRole + '")').length > 0 ){
 				console.log('DEBBUGGING: wsData.passedInRole = ' + wsData.passedInRole);
+				wsData.passedInRole = wsData.passedInRole.replace(/\s/g,'&nbsp;');
 				jQuery('select#get-info-Title option').filter(function () { return jQuery(this).html() === wsData.passedInRole; }).prop('selected',true)
 				wsData.toggleAll();
 			}
