d5bafbcf9f3f88bf6dd8c42b7948c52a7ef555ea
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-11-10 10:15:42 -0500

    shrink .prominent-link for header to .825

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2015-11-09
+/*! WorldStrides - v0.1.0 - 2015-11-10
  * http://www.worldstrides.com
  * Copyright (c) 2015; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
