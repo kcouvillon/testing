8e6b2bf146dd369b188f5f2bc1b13b4046149e65
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-12-23 10:33:09 -0500

    now pulling in ILSEO PowerReviews - quick n dirty

--- includes/class-powerreviews.php
+++ includes/class-powerreviews.php
@@ -45,11 +45,11 @@ class WS_PowerReviews {
 		
 		$response = wp_remote_get( self::$powerreviews_url . 'pwr/mx54y728/rawdata/review_data_summary.xml' );
 		if( is_array($response) ) {
-		  $header = $response['headers']; // array of http header lines
-		  $body = $response['body']; // use the content
+			// $header = $response['headers']; // array of http header lines
+			$body = $response['body']; // use the content
+			self::$_xml = simplexml_load_string( $body );
 		}
 
-		self::$_xml = simplexml_load_string( $body );
 
 		// if( WP_DEBUG ){
 		// $json = json_encode( $xml );
@@ -69,17 +69,72 @@ class WS_PowerReviews {
 	 * Pull out the associated HTML referred to in the XML
 	 */
 	public static function html_from_pr_page_id( $pr_page_id )  {
-		$i = 0;
-		$review_url = '';
+		$review_uri = '';
 		$review_html = '';
+		$product = null;
 
-		while( $product = self::$_xml->product[$i++] ) {
-			if( $pr_page_id == $product->pageid ) {
-				$review_url = $product->inlinefiles->inlinefile;
-			}
+		if( !( $product = self::product_obj_from_pr_page_id( $pr_page_id ) ) ) {
+			return '';
 		}
 
-		return self::$powerreviews_url . $review_url; // @todo: actually return the html
+		try {
+			$review_uri = $product->inlinefiles->inlinefile;
+			$response = wp_remote_get( self::$powerreviews_url . $review_uri );
+			if( is_array($response) ) {
+				// $header = $response['headers']; // array of http header lines
+				$review_html = $response['body']; // use the content
+			}
+		} catch ( Exception $e ) {
+			return ''; // PowerReviews XML parsing badly, eh?
+		}
+
+
+		// think backwards -- prepending the bottom scripts first:
+		// @todo - import these the proper WordPress way
+
+		// @todo - make our own override css?
+		//$review_html .= <link rel="stylesheet" href="https://reviews.worldstrides.com/pwr/engine/merchant_styles2.css" type="text/css" id="prMerchantOverrideStylesheet">';
+
+		// prepend PR styles JS
+		$review_html = '<script type="text/javascript" src="//ui.powerreviews.com/stable/review-display/modern/js/rd-styles.js"></script>' . $review_html;
+		// prepend widget.js script
+		$review_html = '<script type="text/javascript" src="//static.powerreviews.com/widgets/v1/widget.js"></script>' . $review_html;
+		// prepend full.js script
+		$review_html = '<script src="/pwr/engine/js/full.js"></script>' . $review_html;
+		// prepend PR styles CSS
+		$review_html = '<link rel="stylesheet" href="//ui.powerreviews.com/stable/review-display/modern/styles.css" type="text/css" id="prBaseStylesheet">'  . $review_html;
+
+		// then adjust the hyperlinks to work properly:
+		$review_html = preg_replace( '/\/pwr/', self::$powerreviews_url . 'pwr', $review_html ); // prepend reviews.worldstrides.com 
+		$review_html = preg_replace( '/https:/', '', $review_html ); // make all includes start with '//' rather than 'https://'
+		$review_html .= '<script type="text/javascript">POWERREVIEWS.display.engine(document);</script>';
+
+
+
+
+		return $review_html;
+
+	}
+
+	protected static function product_obj_from_pr_page_id( $pr_page_id ) {
+		if( null === self::$_xml ) {
+			return false; // bail!
+		}
+
+		$i = 0;
+		$product_obj = null;
+
+		try {
+			while( $product = self::$_xml->product[$i++] ) {
+				if( $pr_page_id == $product->pageid ) {
+					$product_obj = $product;
+				}
+			}
+		} catch ( Exception $e ) {
+			return false; // PowerReviews XML parsing badly, eh?
+		}
+
+		return $product_obj;
 	}
 
 }
