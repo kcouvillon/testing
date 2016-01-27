426a3c2feba290a0b96d9ecbbab9ed83a693f374
erikrunia <erikrunia@gmail.com>
2016-01-27 13:13:58 -0500

    making blog search work and adding filter by category under filter by tags

--- partials/content-blog-sidebar-search-tags.php
+++ partials/content-blog-sidebar-search-tags.php
@@ -1,8 +1,13 @@
 <div class="search-widget">
 
 	<?php if( ! is_search() ) : ?>
-		<label for="input-search-blog" class="block no-placeholder">Search Blog</label>
-		<input type="search" id="input-search-blog" placeholder="Search Blog">
+
+		<form role="search" method="get" action="<?php echo site_url(); ?>">
+            <label for="input-search-blog" class="block no-placeholder">Search Blog</label>
+            <input type="search" placeholder="Search Blog" name="s">
+            <input type='hidden' name='post_type' value='post' />			
+			<!-- <input type="submit" class="btn-primary search-submit" value="S" style="float:right"> -->
+		</form>
 	<?php endif; ?>
 
 	<h3>Explore by Tag</h3>
@@ -11,6 +16,19 @@
 
 	<?php foreach ( $tags as $tag ) : ?>
 		<a class="btn btn-success" href="<?php echo get_tag_link( $tag->term_id ); ?>"><?php echo $tag->name; ?></a>
+	<?php endforeach; ?>
+
+	<h3>Explore by Category</h3>
+
+	
+    <?php $cats = get_categories( array(
+        'orderby' => 'name',
+        'order'   => 'ASC',
+        'parent'  => 0
+    )); ?>
+
+	<?php foreach ( $cats as $cat ) : ?>
+		<a class="btn btn-primary" href="<?php echo get_category_link( $cat->term_id ); ?>"><?php echo $cat->cat_name; ?></a>
 	<?php endforeach; ?>
 
 </div>
