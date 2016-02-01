4d11c8858fcc78392321965f1da7972c4374df8b
Logan-K <lklingler0@gmail.com>
2016-02-01 17:50:23 -0500

    hiding dates on reviews

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-01-15
+/*! WorldStrides - v0.1.0 - 2016-02-01
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
