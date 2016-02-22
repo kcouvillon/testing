8e82e2cebb14c1ae226f6d464d2469efdf4615fc
Logan-K <lklingler0@gmail.com>
2016-02-22 14:15:06 -0500

    Changing modal Title

--- partials/modal-welcome.php
+++ partials/modal-welcome.php
@@ -10,7 +10,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-keyboard="true" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title"><?php _e( 'Maintenance Window', 'worldstrides' ); ?></h4>
+          <h4 class="modal-title"><?php _e( 'Planned System Maintenance Alert', 'worldstrides' ); ?></h4>
         </div>
         <div class="modal-body">
           <p>
