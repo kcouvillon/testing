862a3cf7612601a44093778f5f99132fbb6afe25
erikrunia <erikrunia@gmail.com>
2016-03-01 13:07:55 -0500

    adding space between subtitle and title and removing date

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-02-29
+/*! WorldStrides - v0.1.0 - 2016-03-01
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
--- partials/content-search.php
+++ partials/content-search.php
@@ -13,7 +13,7 @@
 		<header class="entry-header">
             <h3 class="entry-title"><a href="<?php echo get_the_permalink() ?>"><?php echo the_title() ?></a></h3>
 			<div class="entry-meta">
-                <b><time datetime=""><?php the_time( 'F j, Y' ); ?></time></b>
+                <!--<b><time datetime=""><?php the_time( 'F j, Y' ); ?></time></b>-->
                 <?php echo get_post_meta( $post->ID, 'itinerary_subtitle', true ); ?>
 				<?php echo get_the_category_list(); ?>
 			</div>
