f818e12675a4c3550ef8da485f41d7370e287bec
erikrunia <erikrunia@gmail.com>
2016-03-14 00:26:30 -0400

    running grunt on new changes

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-03-13
+/*! WorldStrides - v0.1.0 - 2016-03-14
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
