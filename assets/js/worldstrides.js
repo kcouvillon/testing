9f506c1e61bc66449bf291293fa58ee575a1a257
Logan-K <lklingler0@gmail.com>
2016-04-11 15:43:40 -0400

    Added some additional styling to tiles

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-04-08
+/*! WorldStrides - v0.1.0 - 2016-04-11
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
