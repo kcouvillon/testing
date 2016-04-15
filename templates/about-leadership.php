78995e58a4b31b5326a6d1c915fe3810e1385747
Logan-K <lklingler0@gmail.com>
2016-04-15 11:25:54 -0400

    Moving tags around on leadership page.

--- templates/about-leadership.php
+++ templates/about-leadership.php
@@ -16,7 +16,7 @@ get_header(); ?>
 		<section class="section-content leadership-content">
 			<?php get_template_part( 'partials/content', 'about' ) ?>
 		</section>
-		<p>Programs</p>
+		<p class="section-content" style="font-size: 32px; font-weight:bold">Programs</p>
 		<section class="section-content leadership">
 			<?php $associated_bios = get_post_meta( $post->ID, 'ws_attached_leadership_bios', true ); ?>
 
@@ -40,7 +40,7 @@ get_header(); ?>
 				<?php $group = get_post_meta( $post->ID, 'ws_bio_group', true ); ?>
 				<?php if ($group == 'programs') : ?>
 					<?php $program_count++; ?>
-					<div id="marker" style="display:none"></div>
+
 				<article <?php post_class(); ?>>
 					<header class="entry-header">
 						<?php if ( has_post_thumbnail() ) : ?>
@@ -84,6 +84,110 @@ get_header(); ?>
 			<?php if ($program_count > 0): ?>
 			<br><br>
 			<?php endif; ?>
+			<p style="clear:both">Content</p>
+			<?php ////////////////////////////////////////////////////////////
+			//SECOND ITERATION ?>
+			<?php foreach ( $associated_bios as $bio_id ) : ?>
+				<?php $post = get_post( $bio_id ); ?>
+				<?php setup_postdata($post); ?>
+				<?php $position = get_post_meta( $post->ID, 'ws_bio_position', true ); ?>
+				<?php $group = get_post_meta( $post->ID, 'ws_bio_group', true ); ?>
+
+				<?php if ($group == 'customer_contact') : ?>
+					<?php $customer_contact_count++; ?>
+					<article <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="headshot clearfix">
+									<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'medium' ); ?></a>
+								</div>
+							<?php else : ?>
+								<div class="headshot-pattern <?php echo WS_Helpers::get_random_pattern(); ?>">
+									<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>"></a>
+								</div>
+							<?php endif; ?>
+
+							<h3 class="leader-name entry-title">
+								<a href="<?php the_permalink(); ?>" rel="bookmark">
+									<?php the_title(); ?>
+								</a>
+							</h3>
+
+							<?php if ( $position ) : ?>
+								<span class="entry-position"><?php echo esc_html( $position ); ?></span>
+							<?php endif; ?>
+						</header>
+
+						<div class="entry-content">
+							<?php
+							if ( get_the_excerpt() ) {
+								the_excerpt();
+							} else {
+								the_content();
+							}
+							?>
+						</div>
+
+						<footer class="entry-footer">
+							<a href="<?php the_permalink(); ?>">Read more about <?php the_title(); ?></a>
+						</footer>
+					</article>
+				<?php endif; ?>
+			<?php endforeach; ?>
+
+			<?php if ($customer_contact_count > 0 ): ?>
+
+			<?php endif; ?>
+			<p style="clear:both; font-size:18px">Shared</p>
+			<?php /////////////////////////////////////////////////////
+			/////////////////////THIRD ITERATION ?>
+			<!--<h2>Shared Support</h2>-->
+			<?php foreach ( $associated_bios as $bio_id ) : ?>
+				<?php $post = get_post( $bio_id ); ?>
+				<?php setup_postdata($post); ?>
+				<?php $position = get_post_meta( $post->ID, 'ws_bio_position', true ); ?>
+				<?php $group = get_post_meta( $post->ID, 'ws_bio_group', true ); ?>
+
+				<?php if ($group == 'shared_support') : ?>
+					<article <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="headshot clearfix">
+									<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'medium' ); ?></a>
+								</div>
+							<?php else : ?>
+								<div class="headshot-pattern <?php echo WS_Helpers::get_random_pattern(); ?>">
+									<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>"></a>
+								</div>
+							<?php endif; ?>
+
+							<h3 class="leader-name entry-title">
+								<a href="<?php the_permalink(); ?>" rel="bookmark">
+									<?php the_title(); ?>
+								</a>
+							</h3>
+
+							<?php if ( $position ) : ?>
+								<span class="entry-position"><?php echo esc_html( $position ); ?></span>
+							<?php endif; ?>
+						</header>
+
+						<div class="entry-content">
+							<?php
+							if ( get_the_excerpt() ) {
+								the_excerpt();
+							} else {
+								the_content();
+							}
+							?>
+						</div>
+
+						<footer class="entry-footer">
+							<a href="<?php the_permalink(); ?>">Read more about <?php the_title(); ?></a>
+						</footer>
+					</article>
+				<?php endif; ?>
+			<?php endforeach; ?>
 
 
 	</main>
