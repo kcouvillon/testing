255962b0b87698854b99a80b937832e7f24dc7ba
Logan-K <lklingler0@gmail.com>
2016-04-19 15:14:15 -0400

    Still needed a little styling.

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-04-12
+/*! WorldStrides - v0.1.0 - 2016-04-19
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
