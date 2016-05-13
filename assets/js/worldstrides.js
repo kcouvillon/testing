529449a62ab77921b25e46efa30804903c060fcf
Logan-K <lklingler0@gmail.com>
2016-05-13 15:16:38 -0400

    css fix

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-05-11
+/*! WorldStrides - v0.1.0 - 2016-05-13
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
