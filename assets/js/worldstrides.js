c3cb610496d7bc4de6024ad7dcee23cdc3f1af33
Logan-K <lklingler0@gmail.com>
2016-05-18 12:00:27 -0400

    Grunting

--- assets/js/worldstrides.js
+++ assets/js/worldstrides.js
@@ -1,4 +1,4 @@
-/*! WorldStrides - v0.1.0 - 2016-05-13
+/*! WorldStrides - v0.1.0 - 2016-05-18
  * http://www.worldstrides.com
  * Copyright (c) 2016; * Licensed GPLv2+ */
 ( function( $, window, undefined ) {
